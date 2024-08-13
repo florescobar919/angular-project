45ab383b2e5755724dedbdc05dc6cffc431ca086
Flor Escobar <florescobar919@gmail.com>
2024-08-13 10:14:36 +0200

    this is my solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'to-do';
   users = DUMMY_USERS;
+  user : any;
+  userName: string = "";
 
   selectedUser(idUser: string) {
     console.log("the user clicked is: " + idUser);    
+    this.user = this.users.find(user => user.id === idUser);
+    this.userName = this.user.name;
   }
+
+
 }
