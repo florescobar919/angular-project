37a11c3a132f28ee0691920c37df698f2da2350b
Flor Escobar <florescobar919@gmail.com>
2024-08-13 11:59:21 +0200

    45. Outputting User-specific Tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,4 +10,41 @@ import { TaskComponent } from "./task/task.component";
 })
 export class TasksComponent {
   @Input({required: true}) userName!: string;
+  @Input({required: true}) userId!: string;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId)
+  }
+
+  tasks = [
+    {
+      id: "t1",
+      userId: "u1",
+      title: "Master Angular",
+      summary: "Learn all the basic and davanced features of Angular & how to apply them",
+      dueDate: "2025-12-12"
+    },
+    {
+      id: "t1",
+      userId: "u3",
+      title: "Master Angular",
+      summary: "Learn all the basic and davanced features of Angular & how to apply them",
+      dueDate: "2025-12-12"
+    },
+    {
+      id: "t1",
+      userId: "u4",
+      title: "Master Angular",
+      summary: "Learn all the basic and davanced features of Angular & how to apply them",
+      dueDate: "2025-12-12"
+    },
+    {
+      id: "t1",
+      userId: "u2",
+      title: "Master Angular",
+      summary: "Learn all the basic and davanced features of Angular & how to apply them",
+      dueDate: "2025-12-12"
+    }
+  ]
+
 }
